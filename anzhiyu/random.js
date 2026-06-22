11ce9f52d8aac1ef1f97af10386d66cfe7b85896
40yebufu <john883636@gmail.com>
2026-06-22 15:57:13 +0800

    Site updated: 2026-06-22 15:57:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/14/hello-world/"];function toRandomPost(){
+var posts=["2026/06/19/中年爸爸的撸铁日常（一）/","2026/06/18/冲刺奥数历险记（一)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
